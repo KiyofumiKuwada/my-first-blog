98cc7f07c5426aaba3b92fe98b79aea35d8a65fa
qolservice <kuwada@ms6.megaegg.ne.jp>
2019-06-10 18:02:35 +0900

    all Changed for site.

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,19 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
-def post_list(request):
-    return render(request, 'blog/post_list.html',{})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html',{'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -32,7 +27,6 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -46,6 +40,5 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
 
 # Create your views here.
